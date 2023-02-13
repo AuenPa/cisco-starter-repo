2f8c8dd5fd4694491b37b79ecfb28a1d3cf0fcb1
AuenPa <angelrangel2695@gmail.com>
2023-02-13 14:01:34 -0600

    added tab windows and title

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. Hello world!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [state, setState] = useState(1);
+
+    const action = (index) => {
+        setState(index)
+    }
+
+    return (
+       <div className="App">
+            <div className="App-header">
+                <h1>Sextant</h1>
+            </div>
+
+           <div className="box">
+
+               <div className="tabs">
+                   <div onClick={() => action(1)} className={`${state === 1 ?
+                       'tab active-tab' : 'tab'}`}>
+                       tab1
+                   </div>
+
+                   <div onClick={() => action(2)} className={`${state === 2 ?
+                       'tab active-tab' : 'tab'}`}>
+                       tab2
+                   </div>
+
+                   <div onClick={() => action(3)} className={`${state === 3 ?
+                       'tab active-tab' : 'tab'}`}>
+                       tab3
+                   </div>
+               </div>
+               { /* contents */}
+               <div className="contents">
+                   <div className={`${state === 1 ?
+                       "content active-content" : "content"}`}>
+                       <h2>content1</h2>
+                       <p>this is content 1</p>
+                   </div>
+                   <div className={`${state === 2 ?
+                       "content active-content" : "content"}`}>
+                       <h2>content2</h2>
+                       <p>this is content 2</p>
+                   </div>
+                   <div className={`${state === 3 ?
+                       "content active-content" : "content"}`}>
+                       <h2>content3</h2>
+                       <p>this is content 3</p>
+                   </div>
+
+               </div>
+           </div>
+
+
+      </div>
+    );
 }
 
 export default App;
